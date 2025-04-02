4b27e6cdc70a85d27c218b7db501c56444183948
MSJISHNU <jishnums9202@gmail.com>
2025-04-02 18:56:40 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,18 @@
+#even or ODD
+
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+{
+int no;
+Printf("enter the number");
+scanf("%d",&no);
+if(no%2==0)
+  printf("Even");
+else
+  printf("odd");
+}
+
+
